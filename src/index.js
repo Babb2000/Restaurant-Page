4b0ba4896b63037a02b60f28a7432fa6cd4d1d6b
Babb Getachew <babbgetachew@gmail.com>
2023-09-12 15:08:34 -0400

    Adding container layout for the first section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import "./style.css";
 
-const logo = document.querySelector("img");
-logo.addEventListener("mouseenter", function(){
-    this.src = "./assets/S.svg";
-})
-
-logo.addEventListener("mouseleave", function(){
-    this.src = "./assets/stsLogo.svg";
-})
