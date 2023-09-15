2bac74ab585739a9a608ca6214b44dadaffe0970
Babb Getachew <babbgetachew@gmail.com>
2023-09-15 11:24:26 -0400

    Added a clearDom feature

---
+++ src/App/Utilities/clearDom.js
@@ -0,0 +1,7 @@
+function clearDom(element){ //Element is the desired element they want to remove from the dom, and page is the page the dom should load to
+
+element.innerHtml = " ";
+
+}
+
+export {clearDom};
