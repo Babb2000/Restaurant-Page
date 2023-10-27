3b50752b994bfe82aea951da776db6a117ad1741
Babb Getachew <babbgetachew@gmail.com>
2023-10-27 14:24:38 -0400

    Add background for menu

--- src/App/menuItems/menu.js
+++ src/App/menuItems/menu.js
@@ -1,3 +1,25 @@
 //Menu Module
+export const menuPage = ()=> {
+
+const mainDiv = document.getElementById("content");
+const backgroundDiv = document.createElement("div");
+backgroundDiv.classList.add("heroImage");
+mainDiv.appendChild(backgroundDiv);
+
+const secondDiv = document.createElement("div");
+secondDiv.classList.add("menuContainer");
+secondDiv.style.width = "80%";
+secondDiv.style.height = "80%";
+secondDiv.style.borderRadius = "30px";
+
+let table = document.createElement("table");
+
+
+secondDiv.appendChild(table);
+backgroundDiv.appendChild(secondDiv);
+mainDiv.appendChild(backgroundDiv);
+
+}
+
 
 
